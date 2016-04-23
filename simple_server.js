3fbefa70b88e2082439f66eabffb6ee89644a088
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-04-23 16:37:41 -0400

    major progress

--- simple_server.js
+++ simple_server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('');
 });
 
 app.post('/addTodo', function(req, res) {
--- test.js
+++ test.js
@@ -9,23 +9,27 @@ cb.insertCloud("http://ec2-54-186-73-1.us-west-2.compute.amazonaws.com");
 // cb.insertCloud("http://ec2-54-186-10-38.us-west-2.compute.amazonaws.com");
 cb.printClouds();
 
+var todo = {
+    'todo' : 'Finish TSE Project'
+}
 
-cb.addRoute("GET", "/", "parameters");
+cb.addRoute("GET", "/");
+cb.addRoute("POST", "/addTodo", todo);
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     console.log(cb.routes);
-    cb.testRoute("GET", "/");
-    cb.testRoute("POST", "/addTodo", )
+    // cb.testRoute("GET", "/");
+    // cb.testRoute("POST", "/addTodo", )
 
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-app.post('/addTodo', )
 
 cb.number_of_trials(100);
 
 // set interval for testing in hours
 cb.interval(6);
+
+cb.startBenchmark();
 
 app.listen(8000);
 console.log("Server started on port 80.");
