2d5d250fd8a45308811cb01a5c2f8efaa32fa97f
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-04-23 13:46:25 -0400

    changed port

--- simple_server.js
+++ simple_server.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(80, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 80!');
 });
