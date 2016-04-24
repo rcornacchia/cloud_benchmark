98fd67d20d7e428129de14ab1590f2895f729de6
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-04-23 22:34:51 -0400

    added req method

--- simple_server.js
+++ simple_server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.get('/', function (req, res) {
     res.send('');
 });
 
 app.post('/addTodo', function(req, res) {
-    console.log(req.param('todo'));  
+    console.log(req.body('todo'));
     res.send("Todo successfully added!");
 });
 
