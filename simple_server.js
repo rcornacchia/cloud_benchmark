929248cdd79479f038b2dcfbf4558ddabcd92a3b
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-04-23 22:40:05 -0400

    adding more routes

--- simple_server.js
+++ simple_server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 
+var todos = new Array();
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -13,8 +14,20 @@ app.get('/', function (req, res) {
 });
 
 app.post('/addTodo', function(req, res) {
-    console.log(req.body('todo'));
+    console.log(req.body.todo);
+    todos.push(req.body.todo);
+    todos.splice(index, 1);
+    console.log("Todos list: " + todos);
+
     res.send("Todo successfully added!");
+});
+
+app.post('/removeTodo', function(req, res) {
+    console.log(req.body.todo);
+    var index = todos.indexOf(5);
+    todos.splice(index, 1);
+    console.log("Todos list: " + todos);
+    res.send("Todo successfully removed.");
 });
 
 app.listen(80, function () {
